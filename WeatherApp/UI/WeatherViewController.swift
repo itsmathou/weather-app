406f53fc89834dc42a35ff42482a0f09887a531d
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2019-01-30 15:39:11 +0000

    first commit

---
+++ WeatherApp/UI/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Mathilde FERRAND on 30/01/2019.
+//  Copyright © 2019 Mathilde FERRAND. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
